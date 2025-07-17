a1f3c516a2e63fa5ef7b8bd4fc17873836a0bcc3
Sarathi <sarathipartha6015@gmail.com>
2025-07-17 12:50:29 +0530

    partha commit

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,22 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 // import router
-const signupRouter=require('./route/signupRoute')
-const studentRouter=require('./route/studentsRoute')
-const signinRoute=require('./route/signinRoute')
+const signupRouter = require("./route/signupRoute");
+const studentRouter = require("./route/studentsRoute");
+const signinRoute = require("./route/signinRoute");
+const student_markRoute = require("./route/student_markRoute");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/signup',signupRouter)
-app.use('/student',studentRouter)
-app.use('/signin',signinRoute)
+app.use("/signup", signupRouter);
+app.use("/student", studentRouter);
+app.use("/signin", signinRoute);
+app.use("/student_mark", student_markRoute);
 
-app.use(
-  morgan( 'dev'
-  )
-);
+app.use(morgan("dev"));
 
 app.listen(5010, () => {
   console.log("Server running on http://localhost:5010");
